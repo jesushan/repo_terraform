27cd7cbf8216fb239df57077ced643914a89cc07
jesushan <43213641+jesushan@users.noreply.github.com>
2022-11-04 10:12:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-######### AWS PROVIDER ##########
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.38.0"
-    }
-  }
-}
-
 
 provider "aws" {
   region     = "us-east-1"
