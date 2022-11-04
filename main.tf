6ef1b9ad18d3918fa2d9eb1969de602c397a3652
jesushan <43213641+jesushan@users.noreply.github.com>
2022-11-04 11:27:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
 }
-
  
-############## BACKEND IS TERRAFORM CLOUD #########
-terraform {
-  backend "remote" {
-    organization = "sun_org"
-
-    workspaces {
-      name = "sun_workspace"
-    }
-  }
-}
-
 ############## CRATE EC2 INSTANCE ###########
 resource "aws_instance" "dev-webserver" {
   ami           = "ami-026b57f3c383c2eec"
