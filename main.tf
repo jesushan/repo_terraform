a9db4939130fee6982f6eb4e6b64d30e801b6487
jesushan <43213641+jesushan@users.noreply.github.com>
2022-11-04 12:56:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 ############## CRATE EC2 INSTANCE ###########
 resource "aws_instance" "dev-webserver" {
   ami           = "ami-026b57f3c383c2eec"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
 }
 
