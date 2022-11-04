6b3075a704a34c6236b2c9ff3da5e9a92ce57fdf
Shan Jesu <shan.jesu@demo.com>
2022-11-03 23:17:04 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+######### AWS PROVIDER ##########
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.38.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAXH3EJPEM5RDNTL6L"
+  secret_key = "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"
+}
+
+############## BACKEND IS TERRAFORM CLOUD #########
+terraform {
+  backend "remote" {
+    organization = "sun_org"
+
+    workspaces {
+      name = "sun_workspace"
+    }
+  }
+}
+
+############## CRATE EC2 INSTANCE ###########
+resource "aws_instance" "dev-webserver" {
+  ami           = "ami-026b57f3c383c2eec"
+  instance_type = "t2.micro"
+}
+
